08cb5bfad72b10269acee58b8e0a5ecad55f2f5b
fatgranddad <162415513+fatgranddad@users.noreply.github.com>
2024-04-08 00:11:11 +0900

    データベースに出品情報を記録

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,12 +10,16 @@ class ItemsController < ApplicationController
   end
 
   def create
-    Item.create(item_params)
-    redirect_to '/'
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path, notice: '商品が正常に出品されました。'
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
   def item_params
-    params.require(:item).permit(:name, :image, :description, :category_id, :condition_id, :shipping_payer_id, :shipping_from_id, :shipping_days_id, :price)
+    params.require(:item).permit(:name, :image, :description, :category_id, :condition_id, :shipping_payer_id, :shipping_from_id, :shipping_days_id, :price).merge(user_id: current_user.id)
   end
 end
