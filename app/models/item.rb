8d35bed4820ccf57f3212dca2e61d6c655f1a7e5
fatgranddad <162415513+fatgranddad@users.noreply.github.com>
2024-04-08 10:52:49 +0900

    アソシエーションを追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -20,4 +20,5 @@ class Item < ApplicationRecord
   validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
 
   has_one_attached :image
+  belongs_to :user
 end
