cafadf6891f7ab5c49841e2ccad8fd6558b857fe
fatgranddad <162415513+fatgranddad@users.noreply.github.com>
2024-04-09 17:52:41 +0900

    itemsコントローラーにedit・update追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,12 +1,13 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update]
+  before_action :redirect_unless_author, only: [:edit, :update]
 
   def index
     @items = Item.all.order('created_at DESC')
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def new
@@ -22,10 +23,29 @@ class ItemsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to item_path(@item), notice: '商品情報が更新されました。'
+    else
+      render :edit
+    end
+  end
+
   private
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
 
   def item_params
     params.require(:item).permit(:name, :image, :description, :category_id, :condition_id, :shipping_payer_id, :shipping_from_id,
                                  :shipping_days_id, :price).merge(user_id: current_user.id)
+  end
+
+  def redirect_unless_author
+    redirect_to root_path unless current_user.id == @item.user_id
   end
 end
