9915c64747dd426cb24b8568dd072c9bbb0ed521
fatgranddad <162415513+fatgranddad@users.noreply.github.com>
2024-04-10 15:02:54 +0900

    api設定_鍵設定前

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,3 +7,4 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 
 pin "new_item_price", to: "new_item_price.js"
+pin "card", to: "card.js"
