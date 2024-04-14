4dc6e40985b476bcfd8d364f4084ad97efde93a5
fatgranddad <162415513+fatgranddad@users.noreply.github.com>
2024-04-14 17:43:32 +0900

    presence: trueをまとめる
    with_options presence: true do ~ endでまとめた

--- app/models/purchase_form.rb
+++ app/models/purchase_form.rb
@@ -3,12 +3,14 @@ class PurchaseForm
   attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :street_address, :building_name, :phone_number,
                 :card_token
 
-  validates :postal_code, presence: true, format: { with: /\A\d{3}-\d{4}\z/ } # 郵便番号は、「123-4567」の形式に一致する必要があります（3桁の数字、ハイフン、4桁の数字）。
-  validates :prefecture_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
-  validates :city, presence: true
-  validates :street_address, presence: true
-  validates :phone_number, presence: true, format: { with: /\A\d{10,11}\z/ }  # 電話番号は、「09012345678」または「0312345678」の形式に一致する必要があります（10桁または11桁の数字）。
-  validates :card_token, presence: true
+  with_options presence: true do
+    validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/ } # 郵便番号は、「123-4567」の形式
+    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+    validates :city
+    validates :street_address
+    validates :phone_number, format: { with: /\A\d{10,11}\z/ }  # 電話番号は、「09012345678」または「0312345678」の形式
+    validates :card_token
+  end
 
   def save
     return false unless valid?
