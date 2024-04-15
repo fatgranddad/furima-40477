cd42530998e0a39a05eb8c00967d21784423ded0
fatgranddad <162415513+fatgranddad@users.noreply.github.com>
2024-04-15 11:44:33 +0900

    sellアクション削除
    ルーティングファイルにて使用していないsellアクションの記述を削除

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
   resources :items do
-    member do
-      patch :sell  # ここにsellアクションへのルーティングを追加
-    end
     resources :orders, only: [:index, :show, :create] 
   end
 end
